e105b88c35aff7cf317032e3c2ea6bae133021cb
Didac Bigorda <didacbigorda@gmail.com>
2019-05-13 16:46:36 -0400

    Splash screen creation

---
+++ src/main.js
@@ -0,0 +1,64 @@
+import { element, grab, DOMScrubber } from '../lib/DOMHelp';
+// import { Game } from './game';
+
+const main = () => {
+
+  const buildSplashScreen = () => {
+    grab('app').append(element({
+      tagName: 'div',
+      attributes: {
+        id: 'splash-screen',
+      },
+      innerHTML: [
+        {
+          tagName: 'h1',
+          attributes: {
+            id: 'game-title'
+          },
+          text: 'Vib-Tie'
+        },
+        {
+          tagName: 'div',
+          attributes: {
+            id: 'start-buttons'
+          },
+          innerHTML: [
+            {
+              tagName: 'button',
+              attributes: {
+                id: 'start-game-button',
+                class: 'menu-buttons'
+              },
+              text: 'Start'
+            },
+          ]
+        }
+      ]
+    }));
+    
+    grab('start-game-button').addEventListener('click', startGame);
+  }
+
+  const startGame = () => {
+
+    DOMScrubber(['splash-screen']);
+
+    grab('app').append(element({
+      tagName: 'canvas',
+      attributes: {
+        id: 'canvas',
+        width: '100%',
+        height: '100%'
+      },
+    }));
+
+    const canvas = grab('canvas');
+    // const game = new Game(canvas);
+// 
+    // game.start();
+  }
+
+  buildSplashScreen();
+}
+
+window.addEventListener('load', main);
